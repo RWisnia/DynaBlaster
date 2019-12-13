97711d186d521f8ab15ff0cb47c06531c2f60f5b
RWisnia <R.Wisnia@tlen.pl>
2019-12-13 10:46:38 +0100

    Add collision detect

--- main.c
+++ main.c
@@ -9,14 +9,12 @@
 #define TEXTURE_PATH "D:\\Aplikacje\\DynaBlaster\\bin\\Debug\\game.png"
 
 SDL_Texture *initialize_texture_from_file(const char* file_name, SDL_Renderer *renderer);
+float WidthField = WIDTH/(float)(FIELDS+2);
+float HeightField = HEIGHT/(float)(FIELDS+2);
 
 int main (int argc, char *argv[]) {
     // Inicjalizacja SDL
     SDL_Init(SDL_INIT_VIDEO);
-
-    // Rozmiar jednego pola
-    float WidthField = WIDTH/(float)(FIELDS+2);
-    float HeightField = HEIGHT/(float)(FIELDS+2);
 
     // Utworzenie okienka
     SDL_Window *window = SDL_CreateWindow("Dyna Blaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
@@ -33,10 +31,10 @@ int main (int argc, char *argv[]) {
     SpriteImage = initialize_texture_from_file( IMG_PATH, renderer);
 
     SDL_Rect character;
-    character.x = WidthField;
-    character.y = HeightField;
-    character.w = WidthField;
-    character.h = HeightField;
+    character.x = WidthField+5;
+    character.y = HeightField+5;
+    character.w = WidthField*0.8;
+    character.h = HeightField*0.8;
 
     int running = 1;
     SDL_Event event;
@@ -52,7 +50,7 @@ int main (int argc, char *argv[]) {
 
             switch (event.key.keysym.sym)
             {
-                case SDLK_LEFT:  character.x-=5; break;
+                case SDLK_LEFT:  character.x-=4; break;
                 case SDLK_RIGHT: character.x+=5; break;
                 case SDLK_UP:    character.y-=5; break;
                 case SDLK_DOWN:  character.y+=5; break;
@@ -61,10 +59,13 @@ int main (int argc, char *argv[]) {
         // Background
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, image_texture, NULL, NULL);
-
+        int coll = detect_collision(character);
+        if(coll)
+        {
+            printf("KOLIZJA!!!");
+        }
         // Player
-        SDL_Rect rect = { character.x, character.y, character.w, character.h };
-        SDL_RenderCopyEx(renderer, SpriteImage, NULL, &rect, 0, NULL, 1);
+        SDL_RenderCopyEx(renderer, SpriteImage, NULL, &character, 0, NULL, 1);
 
         SDL_RenderPresent(renderer);
         SDL_Delay(10);
@@ -77,6 +78,33 @@ int main (int argc, char *argv[]) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
+    return 0;
+}
+
+int detect_collision(SDL_Rect rect){
+    if(detect_collision_point(rect.x, rect.y)) //left up corner
+        return 1;
+    if(detect_collision_point(rect.x+rect.w, rect.y)) // right up corner
+        return 1;
+    if(detect_collision_point(rect.x, rect.y+rect.h)) // left bottom corner
+        return 1;
+    if(detect_collision_point(rect.x+rect.w, rect.y+rect.h)) // right bottom corner
+        return 1;
+
+    return 0;
+}
+
+int detect_collision_point(int x, int y){
+    int current_field_horizontally = (x/WidthField);
+    int current_field_vertical = (y/HeightField);
+
+    if(current_field_horizontally%2==0 && current_field_vertical%2 == 0)
+        return 1;
+    if(current_field_horizontally == 0 || current_field_horizontally == (FIELDS+1))
+        return 1;
+    if(current_field_vertical == 0 || current_field_vertical == (FIELDS+1))
+        return 1;
 
     return 0;
 }
